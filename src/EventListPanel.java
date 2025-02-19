f6c61a21f862fa71bb4fb921475194aea938216b
Sezy07 <sethwojcik7@gmail.com>
2025-02-18 21:37:21 -0600

    Upload Classes and Have EventTester Successful

--- src/EventListPanel.java
+++ src/EventListPanel.java
@@ -2,29 +2,44 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashMap;
 
 public class EventListPanel extends JPanel {
     private ArrayList<Event> events;
+    private HashMap<Event, Boolean> completionStatus; // Stores completion status
     private JPanel displayPanel;
     private JComboBox<String> sortDropDown;
-    private JCheckBox filterCompleted;
+    private JCheckBox filterCompleted, filterDeadlines, filterMeetings, removeFilters;
     private JButton addEventButton;
 
     public EventListPanel() {
         events = new ArrayList<>();
+        completionStatus = new HashMap<>();
         setLayout(new BorderLayout());
 
         // Control Panel
         JPanel controlPanel = new JPanel();
-        sortDropDown = new JComboBox<>(new String[]{"Sort by Name", "Sort by Date"});
+        sortDropDown = new JComboBox<>(new String[]{"Sort by Name", "Sort by Date", "Sort by Reverse Name"});
         sortDropDown.addActionListener(e -> sortEvents());
+
         filterCompleted = new JCheckBox("Hide Completed");
+        filterDeadlines = new JCheckBox("Hide Deadlines");
+        filterMeetings = new JCheckBox("Hide Meetings");
+        removeFilters = new JCheckBox("Remove All Filters");
+
         filterCompleted.addActionListener(e -> updateDisplay());
+        filterDeadlines.addActionListener(e -> updateDisplay());
+        filterMeetings.addActionListener(e -> updateDisplay());
+        removeFilters.addActionListener(e -> resetFilters());
+
         addEventButton = new JButton("Add Event");
         addEventButton.addActionListener(e -> new AddEventModal(this));
 
         controlPanel.add(sortDropDown);
         controlPanel.add(filterCompleted);
+        controlPanel.add(filterDeadlines);
+        controlPanel.add(filterMeetings);
+        controlPanel.add(removeFilters);
         controlPanel.add(addEventButton);
         add(controlPanel, BorderLayout.NORTH);
 
@@ -36,14 +51,18 @@ public class EventListPanel extends JPanel {
 
     public void addEvent(Event event) {
         events.add(event);
+        completionStatus.put(event, event instanceof Completable && ((Completable) event).isComplete());
         updateDisplay();
     }
 
     private void sortEvents() {
-        if (sortDropDown.getSelectedItem().equals("Sort by Name")) {
+        String selectedSort = (String) sortDropDown.getSelectedItem();
+        if ("Sort by Name".equals(selectedSort)) {
             events.sort(Comparator.comparing(Event::getName));
-        } else {
+        } else if ("Sort by Date".equals(selectedSort)) {
             events.sort(Comparator.comparing(Event::getDateTime));
+        } else if ("Sort by Reverse Name".equals(selectedSort)) {
+            events.sort(Comparator.comparing(Event::getName).reversed());
         }
         updateDisplay();
     }
@@ -51,12 +70,33 @@ public class EventListPanel extends JPanel {
     private void updateDisplay() {
         displayPanel.removeAll();
         for (Event event : events) {
-            if (filterCompleted.isSelected() && event instanceof Completable && ((Completable) event).isComplete()) {
+            if (filterCompleted.isSelected() && completionStatus.getOrDefault(event, false)) {
                 continue;
             }
-            displayPanel.add(new EventPanel(event));
+            if (filterDeadlines.isSelected() && event instanceof Deadline) {
+                continue;
+            }
+            if (filterMeetings.isSelected() && event instanceof Meeting) {
+                continue;
+            }
+
+            EventPanel eventPanel = new EventPanel(event, this);
+            displayPanel.add(eventPanel);
         }
         displayPanel.revalidate();
         displayPanel.repaint();
+    }
+
+    private void resetFilters() {
+        filterCompleted.setSelected(false);
+        filterDeadlines.setSelected(false);
+        filterMeetings.setSelected(false);
+        updateDisplay();
+    }
+
+    public void markEventComplete(Event event) {
+        if (event instanceof Completable) {
+            completionStatus.put(event, true);
+        }
     }
 }
--- src/EventPanel.java
+++ src/EventPanel.java
@@ -1,34 +1,44 @@
 import javax.swing.*;
 import java.awt.*;
+import java.time.format.DateTimeFormatter;
 
 public class EventPanel extends JPanel {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm a");
     private Event event;
     private JButton completeButton;
 
-    public EventPanel(Event event) {
+    public EventPanel(Event event, EventListPanel eventListPanel) {
         this.event = event;
         setLayout(new GridLayout(0, 1));
         setBorder(BorderFactory.createLineBorder(Color.BLACK));
 
         JLabel nameLabel = new JLabel("Event: " + event.getName());
-        JLabel timeLabel = new JLabel("Date/Time: " + event.getDateTime().toString());
+        JLabel timeLabel = new JLabel("Date/Time: " + event.getDateTime().format(FORMATTER));
 
         add(nameLabel);
         add(timeLabel);
 
         if (event instanceof Meeting) {
             Meeting meeting = (Meeting) event;
-            add(new JLabel("Ends at: " + meeting.getEndDateTime()));
+            add(new JLabel("Ends at: " + meeting.getEndDateTime().format(FORMATTER)));
             add(new JLabel("Location: " + meeting.getLocation()));
         }
 
         if (event instanceof Completable) {
             Completable completableEvent = (Completable) event;
             completeButton = new JButton("Complete");
-            completeButton.addActionListener(e -> {
-                completableEvent.complete();
+
+            // Check stored completion state
+            if (completableEvent.isComplete()) {
                 completeButton.setEnabled(false);
-            });
+            } else {
+                completeButton.addActionListener(e -> {
+                    completableEvent.complete();
+                    eventListPanel.markEventComplete(event);
+                    completeButton.setEnabled(false);
+                });
+            }
+
             add(completeButton);
         }
     }
